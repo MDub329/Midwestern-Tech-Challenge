cecff87e29d61e67007bad94882b81513068eb8d
Matthew Wells <mdub329@gmail.com>
2021-09-09 22:14:52 -0500

    remove padding from ul

--- client/src/components/contact.jsx
+++ client/src/components/contact.jsx
@@ -5,21 +5,11 @@ function Contact() {
     return (
         <div className="contact">
             <link rel="stylesheet" href="/css/contact.css" />
-            <div class="content">
-                <div id="background"></div>
-                <div>
-                    <img src={logo} alt="logo" />
-                    <a href='/'>Home</a>
-                </div>
-                <div class="col-lg-5">
-                    <h1 class="font-weight-light">Contact</h1>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting
-                        industry. Lorem Ipsum has been the industry's standard dummy text
-                        ever since the 1500s, when an unknown printer took a galley of
-                        type and scrambled it to make a type specimen book.
-                    </p>
-                </div>
+            <div class="leftColumn">
+
+            </div>
+            <div class="rightColumn">
+
             </div>
         </div>
     );
